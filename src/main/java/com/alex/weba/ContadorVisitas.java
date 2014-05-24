bc0a6277ed8f5f4f5a0000bbebcad08f3a4bb10c
T107 <T107@PC256>
2014-05-23 21:18:04 -0500

    primer paso

---
+++ src/main/java/com/alex/weba/ContadorVisitas.java
@@ -0,0 +1,15 @@
+
+package com.alex.weba;
+
+public class ContadorVisitas {
+private static int contador;
+
+    public static int getContador() {
+        contador++;
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+}
